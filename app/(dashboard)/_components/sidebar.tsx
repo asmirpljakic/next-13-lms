a395aac9114a9e3b8446a7a2b33a8f16c3afb47f
Asmir <asmirpljakic@gmail.com>
2023-11-30 17:56:44 +0100

    chage sidebar

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,15 +1,15 @@
-import { Logo } from "./logo"
-import { SidebarRoutes } from "./sidebar-routes"
+import { Logo } from './logo';
+import { SidebarRoutes } from './sidebar-routes';
 
 export const Sidebar = () => {
   return (
-    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-      <div className="p-6">
+    <div className='h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm'>
+      <div className='pt-5 pb-5 pl-3 pr-3'>
         <Logo />
       </div>
-      <div className="flex flex-col w-full">
+      <div className='flex flex-col w-full'>
         <SidebarRoutes />
       </div>
     </div>
-  )
-}
+  );
+};
